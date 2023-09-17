2e865defa25d04b142f3cab35e41079bacb0932f
MaElaRo <manuelasakurarommel@yahoo.de>
2023-09-17 21:53:23 +0200

    added bloc

---
+++ lib/features/appointement_scheduler/cubit/appointement_cubit.dart
@@ -0,0 +1,51 @@
+import 'package:f3_appointment_scheduler/common/model/appointment.dart';
+import 'package:f3_appointment_scheduler/common/services/appointment_service.dart';
+import 'package:f3_appointment_scheduler/features/appointement_scheduler/cubit/appointement_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppointmentCubit extends Cubit<AppointmentState> {
+  AppointmentCubit({
+    required AppointmentService appointmentService,
+  })  : _appointmentService = appointmentService,
+        super(AppointmentInitialLoading());
+
+  final AppointmentService _appointmentService;
+
+  ///
+  Future<void> bookAppointment(Appointment appointment) async {
+    emit(AppointmentBookingInProgress());
+
+    try {
+      final isAvailable =
+          await _appointmentService.checkIfAvailable(appointment);
+      if (isAvailable) {
+        final code = await _appointmentService.bookAppointment(appointment);
+        emit(AppointmentBookingSuccess(code));
+      } else {
+        emit(
+          AppointmentBookingFailure(
+            'Slot was already taken. Please choose another slot',
+          ),
+        );
+      }
+    } catch (error) {
+      emit(AppointmentBookingFailure('Booking appointment failed.'));
+    }
+  }
+
+  ///
+  Future<void> removeAppointment(String code) async {
+    emit(AppointmentRemovingInProgress());
+
+    try {
+      await _appointmentService.removeAppointment(code);
+      emit(AppointmentRemovingSuccess());
+    } catch (error) {
+      emit(AppointmentRemovingFailure('Removing appointment failed.'));
+    }
+  }
+
+  void resetState() {
+    emit(AppointmentInitialLoading());
+  }
+}
